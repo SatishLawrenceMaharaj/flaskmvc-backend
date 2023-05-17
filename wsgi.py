a6d1551542433a615d8b4403f0e465ec2d23ff47
SatishLawrenceMaharaj <77852350+SatishLawrenceMaharaj@users.noreply.github.com>
2023-05-17 18:32:53 -0400

    Up

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,15 @@ from flask import Flask
 from flask.cli import with_appcontext
 
 from App.database import create_db
-from App.main import app, migrate
 from App.controllers import ( create_user, get_all_users_json )
 
 from App.controllers.chat import *
 
+from App.database import create_db, get_migrate
+from App.main import create_app
+
+app = create_app()
+migrate = get_migrate(app)
 
 @app.cli.command("init")
 def initialize():
